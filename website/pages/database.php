d90babb6d1db1d00f5b1b01d7d5e296013ef7647
Tony Jose <gmoto1179@gmail.com>
2022-08-20 20:21:18 +0530

    feat(database): initial content
    - for connecting with the db

---
+++ website/pages/database.php
@@ -0,0 +1,9 @@
+<?php
+
+// 000webhost - production 
+if(!($con = mysqli_connect('localhost', 'SECRET_USERID', 'SECRET_USER_PASS', 'id19270730_studyplus'))) {
+	die("Could not connect to db" . mysqli_connect_error());
+}
+
+
+?>
