14eca8eeb190bb210756098682f5e6923ac0fcf7
Tony Jose <gmoto1179@gmail.com>
2022-08-21 18:59:14 +0530

    feat(logout): implement logout page
    - destroy current session and provide link to home page

---
+++ website/pages/logout.php
@@ -0,0 +1,58 @@
+<?php
+
+    require("database.php");
+    session_start();
+    session_destroy();
+?>
+
+<!doctype html>
+<html lang="en">
+
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Study+</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+      <!-- login css -->
+      <link href="../assets/css/login.css" rel="stylesheet">
+   </head>
+
+   <body class="bg-dark text-white">
+
+      <!-- navigation bar -->
+      <nav class="navbar navbar-expand-lg navbar-dark">
+         <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Study+</span>
+         </div>
+      </nav>
+
+
+      <!-- content -->
+
+      <div class="px-4 py-5 my-5 text-center">
+         <h1 class="display-1 fw-bold">Logged out successfully</h1>
+         <br>
+         <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">
+               <a href="../index.html">Click here to go to Home page</a>
+            </p>
+         </div>
+      </div>
+
+     <!-- Footer -->
+     <div class="container">
+         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p class="col-md-4 mb-0">© 2022 Tony</p>
+            <ul class="nav col-md-4 justify-content-end">
+               <li class="nav-item"><a href="../index.html" class="nav-link px-2">Home</a></li>
+               <li class="nav-item"><a href="about.html" class="nav-link px-2">About</a></li>
+            </ul>
+         </footer>
+      </div>
+
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+   </body>
+
+</html>
