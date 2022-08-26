0096fd4ac6e995f3474faef95c421b04c1810c7c
Tony Jose <gmoto1179@gmail.com>
2022-08-26 20:07:51 +0530

    feat(educatorVideo): add php code
    - display course videos
    - add, modify and delete course video

--- website/pages/educatorVideo.php
+++ website/pages/educatorVideo.php
@@ -6,6 +6,7 @@
         echo "<script>alert('Select a course first.')</script>";
         echo "<script>window.location.replace('educatorHome.php')</script>";
     }
+    $ccode = $_SESSION['course'];
         
     
 ?>
@@ -60,32 +61,39 @@
       <section class="ms-5">
         <?php echo "<h2>Hello, " . $_SESSION['name'] . "</h2>"; ?>
         <section class="ms-5">
-            <h5>Your Videos</h5>
+            <?php echo "<h5>Your Videos in course " . $_SESSION['course'] . "</h5>"; ?>
             <table class="table table-secondary table-striped table-hover table-bordered" style="width: 50%;" >
                 <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Course Code</th>
-                        <th scope="col">Video id</th>
+                        <th scope="col">Video ID</th>
                         <th scope="col">Video name</th>
                         <th scope="col">Video url</th>
+                        <th scope="col">Modify video</th>
+                        <th scope="col">Delete video</th>
                     </tr>
                 </thead>
                 <tbody>
                 <?php 
                     $mail = $_SESSION['mail'];
-                    $qr = "SELECT * FROM educator_code WHERE email='$mail'";
+                    $qr = "SELECT * FROM educator_video WHERE course_code='$ccode' ORDER BY video_id";
                     $result = mysqli_query($con,$qr);
                     $n = mysqli_num_rows($result);
-                    $row = mysqli_fetch_row($result);
-                    for($i=1;$i <= $n; $i++, $row = mysqli_fetch_row($result)) {
+                    $row = mysqli_fetch_array($result);
+                    for($i=1;$i <= $n; $i++, $row = mysqli_fetch_array($result)) {
                         echo '<tr>';
                             echo '<th scope="row">' . $i . '</th>';
-                            echo '<td>' . $row[1] . '</td>';
-                            echo '<td>' .$row[2] . '</td>';
+                            echo '<td>' . $row['video_id'] . '</td>';
+                            echo '<td>' .$row['video_name'] . '</td>';
+                            echo '<td>' .$row['video_url'] . '</td>';
                             echo '<td>';
-                                echo '<form method="get" action="educatorHome.php">';
-                                echo '<button class="w-100 btn btn-sm btn-primary" type="submit" name="selCourse" value=' . $row[1]. '>Select</button>';
+                                echo '<form method="post" action="educatorVideo.php">';
+                                echo "<button class='w-100 btn btn-sm btn-primary' type='submit' name='modifyVideo' value='{$row['video_id']}'>Modify</button>";
+                                echo '</form>';
+                            echo '</td>';
+                            echo '<td>';
+                                echo '<form method="post" action="educatorVideo.php">';
+                                echo "<button class='w-100 btn btn-sm btn-primary' type='submit' name='deleteVideo' value='{$row['video_id']}'>Delete</button>";
                                 echo '</form>';
                             echo '</td>';
                         echo '</tr>';
@@ -95,43 +103,77 @@
             </table>
 
             <form method="post" action="<?php echo $_SERVER['PHP_SELF'];  ?>" enctype="multipart/form-data">
-                <button class="btn btn-sm btn-primary" type="submit" name="createCourse">Add new course</button>
-            </form>
+                <button class="btn btn-sm btn-primary" type="submit" name="AddVideo">Add new video</button>
+           </form>
 
             <?php
-                if(isset($_POST['createCourse']) || isset($_POST['courseCreated'])) {
-                    echo '<form class="form-signin w-100 m-auto" method="post" action="educatorHome.php" enctype="multipart/form-data">';
+
+                if(isset($_POST['modifyVideo']) || isset($_POST['videoModified'])) {
+                    echo '<form class="form-signin w-100 m-auto" method="post" action="educatorVideo.php" enctype="multipart/form-data">';
+                        $videoToModify = $_POST['modifyVideo'];
+                        echo "Editing video with ID: " . $videoToModify;
+                        $qr = "SELECT * FROM educator_video WHERE video_id='{$_POST['modifyVideo']}'";
+                        $result = mysqli_query($con, $qr);
+                        $row = mysqli_fetch_array($result);
                         echo '<div class="form-floating">';
-                            echo '<input type="text" class="form-control" id="floatingInput" placeholder="Code" name="code" required>';
-                            echo '<label class="text-muted" for="floatingInput">Code</label>';
+                            echo "<input type='text' class='form-control' id='floatingInput' placeholder='{$row['video_name']}' name='videoName' required>";
+                            echo "<label class='text-muted' for='floatingInput'>" . $row['video_name'] . "</label>";
                         echo '</div>';
                         echo '<div class="form-floating">';
-                            echo '<input type="text" class="form-control" id="floatingInput" placeholder="Course Name" name="course" required>';
-                            echo '<label class="text-muted" for="floatingInput">Course Name</label>';
+                            echo "<input type='text' class='form-control' id='floatingInput' placeholder='{$row['video_url']}' name='videoURL' required>";
+                            echo "<label class='text-muted' for='floatingInput'>" . $row['video_url'] . "</label>";
+                            echo "<input type='hidden' class='form-control' id='floatingInput' name='modifyVideo' value='$videoToModify' required>";
                         echo '</div>';
-                        echo '<button class="btn btn-sm btn-primary" type="submit" name="courseCreated">Add</button>';
+                        echo '<button class="btn btn-sm btn-primary" type="submit" name="videoModified">Modify</button>';
                     echo '</form>';
 
-                    if(isset($_POST['courseCreated'])) {
-                        // check if code already in use
-                        $ccode = $_POST['code'];
-                        $qr = "SELECT code from educator_code where code='$ccode'";
-                        $result = mysqli_query($con,$qr);
-
-                        if(mysqli_num_rows($result) > 0) {
-                            echo "<script>alert('The course code already exists.')</script>";
-                            mysqli_free_result($result);
-                        }
-                        else {
-                            $cmail = $_SESSION['mail'];
-                            $ccourse = $_POST['course'];
-                            $qr = "INSERT INTO educator_code (email,code,course) VALUES ('$cmail','$ccode','$ccourse')";
-                            mysqli_query($con,$qr);
-                            echo "<script>alert('Course added successfully.')</script>";
-                            header("location: educatorHome.php");
+                    if(isset($_POST['videoModified'])) {
+                        $modifiedVideoName = $_POST['videoName'];
+                        $modifiedVideoUrl = $_POST['videoURL'];
+                        $videoToModify = $_POST['modifyVideo'];
+                        $qr = "UPDATE educator_video SET video_name='$modifiedVideoName' , video_url='$modifiedVideoUrl' WHERE course_code='$ccode' AND video_id='$videoToModify'";
+                        if(mysqli_query($con,$qr)) {
+                            echo "<script>window.location.replace('educatorVideo.php')</script>";
                         }
                     }
-                    
+                }
+
+                if(isset($_POST['deleteVideo'])) {
+                    $videoToDelete = $_POST['deleteVideo'];
+                    $qr = "DELETE FROM educator_video WHERE course_code='$ccode' AND video_id='$videoToDelete'";
+                    if(mysqli_query($con,$qr)) {
+                        echo "<script>alert('Video deleted.')</script>";
+                        echo "<script>window.location.replace('educatorVideo.php')</script>";
+                    }
+                }
+
+                if(isset($_POST['AddVideo']) || isset($_POST['videoAdded'])) {
+                    echo '<form class="form-signin w-100 m-auto" method="post" action="educatorVideo.php" enctype="multipart/form-data">';
+                        echo '<div class="form-floating">';
+                            echo "<input type='number' class='form-control' id='floatingInput' placeholder='Video ID' name='videoID' required>";
+                            echo "<label class='text-muted' for='floatingInput'>Video ID</label>";
+                        echo '</div>';
+                        echo '<div class="form-floating">';
+                            echo "<input type='text' class='form-control' id='floatingInput' placeholder='Video Name' name='videoName' required>";
+                            echo "<label class='text-muted' for='floatingInput'>Video name</label>";
+                        echo '</div>';
+                        echo '<div class="form-floating">';
+                            echo "<input type='text' class='form-control' id='floatingInput' placeholder='Video URL' name='videoURL' required>";
+                            echo "<label class='text-muted' for='floatingInput'>Video URL</label>";
+                        echo '</div>';
+                        echo '<button class="btn btn-sm btn-primary" type="submit" name="videoAdded">Add video</button>';
+                    echo '</form>';
+
+                    if(isset($_POST['videoAdded'])) {
+                        $addedVideoName = $_POST['videoName'];
+                        $addedVideoUrl = $_POST['videoURL'];
+                        $addedVideoId = $_POST['videoID'];
+                        $qr = "INSERT INTO educator_video (course_code, video_id, video_name, video_url) VALUES ('$ccode', '$addedVideoId', '$addedVideoName', '$addedVideoUrl')";
+                        if(mysqli_query($con,$qr)) {
+                            echo "<script>alert('Video Added.')</script>";
+                            echo "<script>window.location.replace('educatorVideo.php')</script>";
+                        }
+                    }
                 }
             ?>
 
